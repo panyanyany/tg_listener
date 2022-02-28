e208493484e6a3223446d47bc18ea88d15da9c66
panyanyany <568397440@qq.com>
2022-02-28 16:17:19 +0800

    可拿到swap交易中的转账记录

---
+++ tg_listener/models/TradeRecord.py
@@ -0,0 +1,22 @@
+import arrow
+from peewee import CharField, IntegerField, DateField, DecimalField
+
+import settings
+
+from tg_listener.models.models import BaseModel, AddressRecord
+
+
+class TradeRecord(BaseModel):
+    token_address = CharField()
+    from_address = CharField()
+    to_address = CharField()
+    direction = CharField()
+
+    D_BUY = 'BUY'
+    D_SELL = 'SELL'
+
+    class Meta:
+        indexes = (
+            (('address', 'day'), True),
+        )
+
