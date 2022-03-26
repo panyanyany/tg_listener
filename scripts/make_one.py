526ef0be9d6588494957aad200356c11fb110497
panyanyany <568397440@qq.com>
2022-03-26 16:21:34 +0800

    add

--- scripts/make_one.py
+++ scripts/make_one.py
@@ -1,6 +1,7 @@
-import sys
+import click
 from pathlib import Path
 
+import pandas
 import pandas as pd
 
 from tg_listener.repo import tick_maker
@@ -9,16 +10,22 @@ from tg_listener.repo.arctic_repo.arctic_repo import arctic_db
 pd.set_option('display.width', 1000)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('max_colwidth', None)
+
+
 # pd.set_option('display.max_rows', None)
 
-token = sys.argv[-1]
 
-maker = tick_maker.TickMaker([
-    tick_maker.TickMakerTask('15min', 0.1),
-])
-maker.debug_token = token.lower()
-maker.run()
+@click.command()
+@click.argument('token')
+@click.option('--span', default='15min')
+def insight(token: str, span='15min'):
+    token = token.lower()
+    tot_data: pandas.DataFrame = arctic_db.db_tick.read(f'{token}:tick')
+    data = tot_data.resample(span)['price'].agg(['first', 'last']).dropna()
+    data['times'] = (data['last'] - data['first']) / data['first']
 
-# for key in maker.results:
-#     result = maker.results[key]
-#     result.save(Path(f'public'))
+    print(data.iloc[-10:])
+
+
+if __name__ == '__main__':
+    insight()
