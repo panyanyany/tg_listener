3ff91e0cb494f6f063229274aa1e1dbf620bdc7c
panyanyany <568397440@qq.com>
2022-04-04 11:22:18 +0800

    分红币规则变更

--- tg_listener/repo/arctic_repo/arctic_repo.py
+++ tg_listener/repo/arctic_repo/arctic_repo.py
@@ -59,7 +59,7 @@ class ArcticRepo:
         stat = stats.find_one(cond)
         return stat
 
-    def update_stat(self, token, last_tick_at=None, pools=None, is_dividend=False, name=None, symbol=None, lps=None):
+    def update_stat(self, token, last_tick_at=None, pools=None, is_dividend=None, name=None, symbol=None, lps=None):
         stats: Collection = self.db_data.stats
         cond = {"token": token}
         stat = stats.find_one(cond)
@@ -74,7 +74,7 @@ class ArcticRepo:
         if lps:
             stat.setdefault('lps', {})
             stat['lps'].update(lps)
-        if is_dividend:
+        if is_dividend is not None:
             stat['is_dividend'] = is_dividend
         if name:
             stat['name'] = name
